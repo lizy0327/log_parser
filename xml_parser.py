86fa70ec2c848ac69ab36821ab40e35f9f4f790e
lizy0327 <lizy0327@gmail.com>
2022-04-27 00:33:15 +0800

    修改遍历xml为for循环方式

--- xml_parser.py
+++ xml_parser.py
@@ -13,15 +13,8 @@
 
 import csv
 import os
-import time
-
 import xml.etree.ElementTree as ET
-
-from xml.etree.ElementTree import parse
-
 # import pandas as pd
-
-# namespaces = {'ns': 'raml20.xsd'}
 
 
 def parser_xml():
@@ -29,68 +22,109 @@ def parser_xml():
     res_list = []
 
     # file_path = str("D:\\tmp\\xmldata.xml").replace("\\", "/")
-    file_path = str("D:\\tmp\\test.xml").replace("\\", "/")
-    # file_path = str("D:\\tmp\\demo.xml").replace("\\", "/")
+    # file_path = str("D:\\tmp\\test.xml").replace("\\", "/")
+    file_path = str("D:\\tmp\\lizy-demo.xml").replace("\\", "/")
     # file_path = str("D:\\tmp\\b.xml").replace("\\", "/")
     # file_path = str("D:\\tmp\\csdn.xml").replace("\\", "/")
     print(file_path)
     tree = ET.parse(file_path)
-    print(tree)
     # 根节点
     root = tree.getroot()
     # 标签名
     print('root_tag:', root)
-    # print("------------------------")
-
-    # 初始化System字典
-    sys_dict = {}
-    sys_dict['Provider Name'] = root[0][0][0].attrib['Name']
-    sys_dict['Provider Guid'] = root[0][0][0].attrib['Guid'][1:-1]
-    sys_dict['EventID'] = root[0][0][1].text
-    sys_dict['EventName'] = root[0][0][2].text
-    sys_dict['Version'] = root[0][0][3].text
-    sys_dict['Source'] = root[0][0][4].text
-    sys_dict['Level'] = root[0][0][5].text
-    sys_dict['Opcode'] = root[0][0][6].text
-    sys_dict['Keywords'] = root[0][0][7].text
-    sys_dict['Result'] = root[0][0][8].text
-    sys_dict['TimeCreated SystemTime'] = root[0][0][9].attrib['SystemTime']
-    sys_dict['Channel'] = root[0][0][11].text
-    sys_dict['Computer'] = root[0][0][12].text
-    sys_dict['ComputerUUID'] = root[0][0][13].text
-
-    # for key, values in sys_dict.items():
-    #     print(key, values)
 
     evt_list = []
+    # 得到event对象
     for events in root:
         evt_dict = {}
 
-        evt_dict['SubjectIP'] = events[1][0].text
-        evt_dict['IPVersion'] = events[1][0].attrib['IPVersion']
-        evt_dict['Name'] = events[1][1].attrib['Name']
+        # print(events.tag)
+        # print(events.attrib)
+        # 得到event里面system和eventdata对象
+        for event in events:
+            # print(event.tag[51:])
+            # print(event.attrib)
 
-        print(events[1][1].attrib)
-        evt_dict['Uid'] = events[1][1].attrib['Uid']
-        evt_dict['Gid'] = events[1][1].attrib['Gid']
-        evt_dict['Local'] = events[1][1].attrib['Local']
+            '''
+            得到systemc和eventdata对象
+            xml对象总共有3种属性：
+            Tag： 使用<和>包围的部分，如<rank>成为start-tag，</rank>是end-tags；
+            Tag结果是一个字符串。
+            Element：被Tag包围的部分，如<rank>68</rank>，可以认为是一个节点，它可以有子节点。element的值通过element.text获得
+            Attribute：在Tag中可能存在的name/value对，如<country name="Liechtenstein">中的name="Liechtenstein"，一般表示属性。
+            Attribute结果是一个字典。
+            '''
+            for item in event:
+                print("tag: ", item.tag[51:])
+                if item.attrib and item.text:
+                    print("attrib: ", item.attrib, type(item.attrib))
+                    print('-----dict-----')
+                    for k, v in item.attrib.items():
+                        print(k,v)
+                    print("element: ", item.text)
+                elif item.attrib:
+                    print("attrib: ", item.attrib, type(item.attrib))
+                    print('-----dict-----')
+                    for k, v in item.attrib.items():
+                        print(k,v)
+                elif item.text:
+                    print("element: ", item.text)
+                # else:
+                #     print(item.tag, "faild.")
+            #     print("attrib: ", item.attrib)
+                # print("element: ", item.attrib)
 
-        evt_dict['SubjectUserSid'] = events[1][2].text
-        evt_dict['SubjectUserIsLocal'] = events[1][3].text
-        evt_dict['SubjectDomainName'] = events[1][4].text
-        evt_dict['SubjectUserName'] = events[1][5].text
-        evt_dict['ObjectServer'] = events[1][6].text
+                # if not bool(item.attrib):
+                #     print("text: ", item.text)
+                # else:
+                #     print("attrib: ", item.attrib, type(item.attrib))
+                    # print(item.text)
 
-        evt_dict['ObjectType'] = events[1][7].text
-        evt_dict['HandleID'] = events[1][8].text
-        evt_dict['ObjectName'] = events[1][9].text
-        evt_dict['InformationRequested'] = events[1][10].text
+
+        # system info
+        # evt_dict['Provider Name'] = events[0][0].attrib['Name']
+        # evt_dict['Provider Guid'] = events[0][0].attrib['Guid'][1:-1]
+        # evt_dict['EventID'] = events[0][1].text
+        # evt_dict['EventName'] = events[0][2].text
+        # evt_dict['Version'] = events[0][3].text
+        # evt_dict['Source'] = events[0][4].text
+        # evt_dict['Level'] = events[0][5].text
+        # evt_dict['Opcode'] = events[0][6].text
+        # evt_dict['Keywords'] = events[0][7].text
+        # evt_dict['Result'] = events[0][8].text
+        # evt_dict['TimeCreated SystemTime'] = events[0][9].attrib['SystemTime']
+        # evt_dict['Channel'] = events[0][11].text
+        # evt_dict['Computer'] = events[0][12].text
+        # evt_dict['ComputerUUID'] = events[0][13].text
+
+
+
+        # event info
+        # evt_dict['SubjectIP'] = events[1][0].text
+        # evt_dict['IPVersion'] = events[1][0].attrib['IPVersion']
+        # evt_dict['Name'] = events[1][1].attrib['Name']
+        #
+        # # print(events[1][1].attrib)
+        # evt_dict['Uid'] = events[1][1].attrib['Uid']
+        # evt_dict['Gid'] = events[1][1].attrib['Gid']
+        # evt_dict['Local'] = events[1][1].attrib['Local']
+        #
+        # evt_dict['SubjectUserSid'] = events[1][2].text
+        # evt_dict['SubjectUserIsLocal'] = events[1][3].text
+        # evt_dict['SubjectDomainName'] = events[1][4].text
+        # evt_dict['SubjectUserName'] = events[1][5].text
+        # evt_dict['ObjectServer'] = events[1][6].text
+        #
+        # evt_dict['ObjectType'] = events[1][7].text
+        # evt_dict['HandleID'] = events[1][8].text
+        # evt_dict['ObjectName'] = events[1][9].text
+        # evt_dict['InformationRequested'] = events[1][10].text
 
         # 把解析的字典格式的数据，添加到list当中
         evt_list.append(evt_dict)
 
         # 在list中，每一条list记录都包含sys_dict和evt_dict这2条记录，其中sys_dict字典内容全部相等
-        res_list.append({**sys_dict, **evt_dict})
+        res_list.append(evt_dict)
 
     return res_list
 
@@ -109,7 +143,7 @@ def write_csv(report_name, res_list, report_path: str = None):
     os.path.abspath(report_path.replace("\\", "/"))
     fileheader = res_list[0]
     path = os.path.join(report_path, report_name)
-    print(path)
+    # print(path)
     with open(path, "w+", newline="") as fp:
         writer = csv.DictWriter(fp, fieldnames=fileheader)
         writer.writeheader()
