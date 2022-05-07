94323cac9dc930be025c67ccfd91634468196089
lizy0327 <lizy0327@gmail.com>
2022-05-08 02:25:26 +0800

    增加可以接收OS参数的方式处理xml文件，并且可以处理目录下多个xml文件。

--- xml_parser.py
+++ xml_parser.py
@@ -11,12 +11,18 @@
 # Description: Welcom!!!
 """
 
+# 一个分析xml日志文件的小工具，使用此工具会把相同的EventName类型的日志保存到同一个csv文件中。
+# 此工具还提供了只分析xml里有几种EventName类型的事件的方法：parser_event_type
+# Copyright LiZhaoYang
+
+import sys
 import csv
 import os
 import time
 import collections
 import xml.etree.ElementTree as ET
 
+version = 0.8
 
 def parser_event_type(xml_path):
     """
@@ -49,70 +55,74 @@ def parser_xml(xml_path):
     :param xml_path: 要处理的xml文件路径
     :return: 所有事件的list集合
     """
+    try:
+        tree = ET.parse(xml_path)
+        # 根节点
+        root = tree.getroot()
+        # 标签名
+        # print('root_tag:', root)
 
-    tree = ET.parse(xml_path)
-    # 根节点
-    root = tree.getroot()
-    # 标签名
-    # print('root_tag:', root)
-    # 存放所有字典数据的list
-    events_list = []
+        # tree = ET.parse(xml_path)
 
-    # 得到event对象合集
-    for events in root:
-        # 声明一个严格按照key插入顺序排序的字典
-        dct_name = collections.OrderedDict()
-        # 得到一个完整的Event，遍历出System和EventData
-        for event in events:
-            # 分别对System和EventData进行遍历，并把数据存储到字典中
-            for item in event:
-                """
-                EventData对象的tag全部是Data，没法区分不同的字段
-                在EventData对象的子节点里有3个比较特殊的字段，attrib有多个值，
-                Object类型事件：<Data Name="SubjectIP" IPVersion="4">10.128.61.231</Data>
-                Object类型事件：<Data Name="SubjectUnix" Uid="65534" Gid="65534" Local="false"/>
-                Logon类型事件：<Data Name="IpAddress" IPVersion="4">10.128.61.232</Data>
-                """
-                # 处理EventData节点数据
-                if item.tag[51:] == "Data":
-                    # Object相关的事件
-                    if item.attrib['Name'] == "SubjectIP":
-                        dct_name['SubjectIP'] = item.text
-                        dct_name['IPVersion'] = item.attrib['IPVersion']
-                    # Object相关的事件
-                    elif item.attrib['Name'] == "SubjectUnix":
-                        dct_name['SubjectUnix'] = "SubjectUnix"
-                        dct_name['UID'] = item.attrib['Uid']
-                        dct_name['GID'] = item.attrib['Gid']
-                        dct_name['Local'] = item.attrib['Local']
-                    # Logon相关的事件
-                    elif item.attrib['Name'] == "IpAddress":
-                        dct_name['IpAddress'] = item.text
-                        dct_name['IPVersion'] = item.attrib['IPVersion']
+        # 存放所有字典数据的list
+        events_list = []
+        # 得到event对象合集
+        for events in root:
+            # 声明一个严格按照key插入顺序排序的字典
+            dct_name = collections.OrderedDict()
+            # 得到一个完整的Event，遍历出System和EventData
+            for event in events:
+                # 分别对System和EventData进行遍历，并把数据存储到字典中
+                for item in event:
+                    """
+                    EventData对象的tag全部是Data，没法区分不同的字段
+                    在EventData对象的子节点里有3个比较特殊的字段，attrib有多个值，
+                    Object类型事件：<Data Name="SubjectIP" IPVersion="4">10.128.61.231</Data>
+                    Object类型事件：<Data Name="SubjectUnix" Uid="65534" Gid="65534" Local="false"/>
+                    Logon类型事件：<Data Name="IpAddress" IPVersion="4">10.128.61.232</Data>
+                    """
+                    # 处理EventData节点数据
+                    if item.tag[51:] == "Data":
+                        # Object相关的事件
+                        if item.attrib['Name'] == "SubjectIP":
+                            dct_name['SubjectIP'] = item.text
+                            dct_name['IPVersion'] = item.attrib['IPVersion']
+                        # Object相关的事件
+                        elif item.attrib['Name'] == "SubjectUnix":
+                            dct_name['SubjectUnix'] = "SubjectUnix"
+                            dct_name['UID'] = item.attrib['Uid']
+                            dct_name['GID'] = item.attrib['Gid']
+                            dct_name['Local'] = item.attrib['Local']
+                        # Logon相关的事件
+                        elif item.attrib['Name'] == "IpAddress":
+                            dct_name['IpAddress'] = item.text
+                            dct_name['IPVersion'] = item.attrib['IPVersion']
+                        else:
+                            """
+                            EventData子节点的tag全部都是Data无法进行区分
+                            子节点的attrib是一个字典格式，而且字典的key也都是Name，这个不需要
+                            最终需要的数据是以：子节点attrib的value作为“key",子节点element作为"value"进行存储
+                            示例：<Data Name="SubjectUserIsLocal">false</Data>
+                            """
+                            for v in item.attrib.values():
+                                dct_name[v] = item.text
+                    # 处理System节点数据
                     else:
                         """
-                        EventData子节点的tag全部都是Data无法进行区分
-                        子节点的attrib是一个字典格式，而且字典的key也都是Name，这个不需要
-                        最终需要的数据是以：子节点attrib的value作为“key",子节点element作为"value"进行存储
-                        示例：<Data Name="SubjectUserIsLocal">false</Data>
+                        在System对象的子节点里有2个比较特殊的字段，只有attrib，没有element，需要特殊处理，示例：
+                        <Provider Name="NetApp-Security-Auditing" Guid="{3CB2A168-FE19-4A4E-BDAD-DCF422F13473}"/>
                         """
-                        for v in item.attrib.values():
-                            dct_name[v] = item.text
-                # 处理System节点数据
-                else:
-                    """
-                    在System对象的子节点里有2个比较特殊的字段，只有attrib，没有element，需要特殊处理，示例：
-                    <Provider Name="NetApp-Security-Auditing" Guid="{3CB2A168-FE19-4A4E-BDAD-DCF422F13473}"/>
-                    """
-                    if item.tag[51:] == "Provider":
-                        dct_name['Provider Name'] = item.attrib['Name']
-                        dct_name['GUID'] = item.attrib['Guid']
-                    elif item.tag[51:] == "TimeCreated":
-                        dct_name['TimeCreated SystemTime'] = item.attrib['SystemTime']
-                    else:
-                        dct_name[item.tag[51:]] = item.text
-        events_list.append(dct_name)
-    return events_list
+                        if item.tag[51:] == "Provider":
+                            dct_name['Provider Name'] = item.attrib['Name']
+                            dct_name['GUID'] = item.attrib['Guid'][1:-1]
+                        elif item.tag[51:] == "TimeCreated":
+                            dct_name['TimeCreated SystemTime'] = item.attrib['SystemTime']
+                        else:
+                            dct_name[item.tag[51:]] = item.text
+            events_list.append(dct_name)
+        return events_list
+    except Exception as e:
+        print(f"the file: {xml_path} is not xml format")
 
 
 def write_csv(report_name, event_list, report_path: str = None):
@@ -166,15 +176,122 @@ def archive_dct(dct_list, report_path):
         write_csv(report_name=k, event_list=v, report_path=report_path)
 
 
+def process_xml(input_path: str = "", output_path: str = ""):
+    '''
+    根据传入的xml文件及生成报告的csv文件目录类型的不同，使用不同的策略。
+    :param input_path: xml文件或文件路径
+    :param output_path: 解析后csv文件路径，如果缺省，则在input_path同级目录下创建新目录
+    :return:
+    '''
+    # 分别传入了xml文件目录和csv报告保存目录
+    if os.path.isdir(input_path) and os.path.isdir(output_path):
+        print("xml dir and output dir")
+        for flines in os.listdir(input_path):
+            if os.path.splitext(flines)[1] == '.xml':
+                time_stamp = time.strftime("%Y%m%d-%H%M%S", time.localtime())
+                # 在目录中根据不同的xml文件名称创建不同的目录
+                new_output_path = os.path.join(
+                    output_path + f"/{os.path.splitext(flines)[0]}" + "_ln_log_parser_" + time_stamp)
+                if not os.path.exists(new_output_path):
+                    os.mkdir(new_output_path)
+                # 把路径和文件拼接成最终路径
+                new_path = os.path.join(input_path, flines).replace("\\", "/")
+                try:
+                    # 解析xml文件
+                    all_evt_list = parser_xml(xml_path=str(new_path).replace("\\", "/"))
+                    # 把解析后的xml写入csv文件中
+                    archive_dct(all_evt_list, report_path=str(new_output_path).replace("\\", "/"))
+                except Exception as e:
+                    os.rmdir(new_output_path)
+                    print(f"the file: {new_path} is bad.")
+    # 只传入了xml文件目录
+    elif os.path.isdir(input_path):
+        print("only xml dir")
+        # print(os.path.abspath(os.path.join(input_path, "../")))
+        for flines in os.listdir(input_path):
+            if os.path.splitext(flines)[1] == '.xml':
+                time_stamp = time.strftime("%Y%m%d-%H%M%S", time.localtime())
+                # 获得上级目录并在同级目录下创建开头相同的文件夹，这里使用的是输入路径的绝对值+后缀的，和普通的join里使用逗号拼接是不一样的
+                up_path = os.path.join(os.path.abspath(input_path) + "_ln_log_parser" + time_stamp)
+                if not os.path.exists(up_path):
+                    os.mkdir(up_path)
+                # 在目录中根据不同的xml文件名称创建不同的目录，这里使用的是逗号拼接的目录
+                new_output_path = os.path.join(
+                    up_path, os.path.splitext(flines)[0] + "_ln_log_parser_" + time_stamp)
+                if not os.path.exists(new_output_path):
+                    os.mkdir(new_output_path)
+                # 把路径和文件拼接成最终路径
+                new_path = os.path.join(input_path, flines).replace("\\", "/")
+                try:
+                    # 解析xml文件
+                    all_evt_list = parser_xml(xml_path=str(new_path).replace("\\", "/"))
+                    # 把解析后的xml写入csv文件中
+                    archive_dct(all_evt_list, report_path=str(new_output_path).replace("\\", "/"))
+                except Exception as e:
+                    os.rmdir(new_output_path)
+                    print(f"the file: {new_path} is bad.")
+    # 传入了xml文件本身，及csv报告保存目录
+    elif os.path.isfile(input_path) and os.path.isdir(output_path):
+        print("xml file and output dir")
+        try:
+            all_evt_list = parser_xml(xml_path=str(input_path).replace("\\", "/"))
+            archive_dct(all_evt_list, report_path=str(output_path).replace("\\", "/"))
+        except Exception as e:
+            os.rmdir(input_path)
+            print(f"the file: {input_path} is bad.")
+    # 只传入了xml文件本身
+    elif os.path.isfile(input_path):
+        print("only xml file")
+        time_stamp = time.strftime("%Y%m%d-%H%M%S", time.localtime())
+        new_output_path = os.path.join(input_path[0:-4] + "_ln_log_parser_" + time_stamp)
+        os.mkdir(new_output_path)
+        try:
+            all_evt_list = parser_xml(xml_path=str(input_path).replace("\\", "/"))
+            archive_dct(all_evt_list, report_path=str(new_output_path).replace("\\", "/"))
+        except Exception as e:
+            os.rmdir(new_output_path)
+            print(f"the file: {input_path} is bad.")
+            print(e)
+    else:
+        print("555555555555555555")
+        print("path is error")
+
+
 if __name__ == '__main__':
     # file_path = str("D:\\tmp\\demo.xml").replace("\\", "/")
-    file_path = str("D:\\tmp\\xmldata.xml").replace("\\", "/")
+    # file_path = str("D:\\tmp\\xmldata.xml").replace("\\", "/")
     # file_path = str("D:\\tmp\\all_type.xml").replace("\\", "/")
     # file_path = str("D:\\tmp\\lizy-demo.xml").replace("\\", "/")
     # file_path = str("D:\\tmp\\b.xml").replace("\\", "/")
     # file_path = str("D:\\tmp\\csdn.xml").replace("\\", "/")
-    parser_event_type(xml_path=file_path)
-    all_evt_list = parser_xml(xml_path=file_path)
-    archive_dct(all_evt_list, report_path="D:\\tmp\\123")
+    # parser_event_type(xml_path=file_path)
+
+    # process_xml(input_path="D:\\tmp\input\\fewfew", output_path="D:\\tmp\\123")
+    # process_xml(input_path="D:\\tmp\input\demo.xml", output_path="D:\\tmp\\123")
+    # process_xml(input_path="D:\\tmp\input\\fewfew\eee.xml", output_path="D:\\tmp\\123")
+    process_xml(input_path="D:\\tmp\input\\fewfew\eee.xml")
+    # process_xml(input_path="D:\\tmp\input\\fewfew\\")
+    # process_xml(input_path="D:\\tmp\\all_type.xml")
+
+    # test1 = "D:\\tmp\input\\fewfew"
+    # print(test1)
+    # # print(os.path.join(os.path.dirname(test1) , "111111" + "22222222"))
+    # print(os.path.join(os.path.abspath(test1)  + "dddd"))
+    # print(os.path.abspath(os.path.join(os.path.abspath(test1) ,   "dddd")))
+    # test2 = os.path.join(test1 + "1111111111" + "00000000000")
+    # print(os.path.join(test2, "222222222"))
+
+    input_arg = sys.argv
+    # print(input_path[0])
+    # print(input_arg)
+    # 判断参数输入是否正确
+    if len(input_arg) == 1:
+        print("the xml file is must")
+    elif len(input_arg) == 2:
+        process_xml(input_path=str(input_arg[1]).replace("\\", "/"))
+    elif len(input_arg) >= 3:
+        process_xml(input_path=str(input_arg[1]).replace("\\", "/"), output_path=str(input_arg[2]).replace("\\", "/"))
+    else:
+        print("arg error")
 
     pass
